f2e555cc21946dc9689f0bc02e26d1f9242e4228
Diana Zhang <dianaz1@andrew.cmu.edu>
2016-12-04 14:18:25 -0500

    OFDM Modulator and Demodulator -- working

--- OFDMMod.m
+++ OFDMMod.m
@@ -1,4 +1,34 @@
 function [toTransmit] = OFDMMod(numSubcarriers, D, msgSym)
+% 
+% %% OFDM Modulator: modulates msgSym into OFDM subcarriers
+% % [numSubcarriers] subcarriers and [D] cyclic prefix length
+% 
+% % IFFT
+% subc = numSubcarriers; % #subcarriers
+% ifftmsg = zeros(ceil(length(msgSym)/subc), subc + D);
+% 
+% % break symbols into chunks of size numSubcarriers
+% for ii = 1:floor(length(msgSym)/subc)
+%     % take fft of each chunk and add cyclic prefix
+%     ifftmsg(ii, D+1:D+subc) = ifft(msgSym(subc*(ii-1)+1:subc*(ii-1)+subc), subc); 
+%     ifftmsg(ii, 1:D) = ifftmsg(ii, end-D+1:end);
+%     fftmsg(ii, 1:subc) = fft(ifftmsg(ii, D+1:subc+D), subc); 
+% end
+% last = msgSym(subc*(ii)+1:end);
+% % zero pad last entry, do the same
+% ifftmsg(ii+1, D+1:D+subc) = ifft([last zeros(1, subc-length(last))], subc);
+% ifftmsg(ii+1, 1:D) = ifftmsg(ii+1, end-D+1:end);
+% fftmsg(ii+1, 1:subc) = fft(ifftmsg(ii+1,D+1:end), subc);
+% 
+% width = length(fftmsg(:, 1));
+% msgsym = zeros(1, width * subc);
+% % concatenate them all into a message
+% for ii = 1:width
+%     msgsym((ii-1)*subc+1:ii*subc) = fftmsg(ii,:);
+% end
+% 
+% end
+
 %% OFDM Modulator: modulates msgSym into OFDM subcarriers
 % [numSubcarriers] subcarriers and [D] cyclic prefix length
 
@@ -7,7 +37,7 @@ subc = numSubcarriers; % #subcarriers
 ifftmsg = zeros(ceil(length(msgSym)/subc), subc + D);
 
 % break symbols into chunks of size numSubcarriers
-for ii = 1:1:length(msgSym)/subc
+for ii = 1:floor(length(msgSym)/subc)
     % take fft of each chunk and add cyclic prefix
     ifftmsg(ii, D+1:D+subc) = ifft(msgSym(subc*(ii-1)+1:subc*(ii-1)+subc), subc); 
     ifftmsg(ii, 1:D) = ifftmsg(ii, end-D+1:end);
@@ -22,6 +52,4 @@ toTransmit = zeros(1, width * (subc+D));
 %concatenate them all into a time signal
 for ii = 1:width
     toTransmit((ii-1)*(subc+D)+1:ii*(subc+D)) = ifftmsg(ii,:);
-end
-
 end
